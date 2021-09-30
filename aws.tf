8c337678f9e36620834d494504beaaae38d935ad
avinash-mahto <aklinuxworld@gmail.com>
2021-09-30 23:41:20 +0800

    added aws.tf file

---
+++ aws.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  profile    = "${var.profile}"
+  region     = "${var.region}"
+}
+
+resource "aws_instance" "web-server" {
+  ami           = "ami-0d058fe428540cd89"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "terraformtraining.com"
+  }
+}
