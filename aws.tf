99324dac67accbdbd02c40d8efa1e8f15208026b
Avinash Mahto <aklinuxworld@gmail.com>
2022-04-24 13:51:28 +0800

    chnages in DB

--- aws.tf
+++ aws.tf
@@ -7,7 +7,7 @@ provider "aws" {
   region     = "${var.region}"
 }
 
-resource "aws_instance" "web-server" {
+resource "aws_instance" "DB-server" {
   ami           = "ami-0d058fe428540cd89"
   instance_type = "t2.micro"
 
